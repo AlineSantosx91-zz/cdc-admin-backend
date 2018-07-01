22ae93993f2a51480dbf2c5a958c0ea97a23037e
dosline <alinerodriguesdossantos@correio.itau.com.br>
2018-07-01 17:00:03 -0300

    adiciona update  e delete

--- src/main/java/br/com/cdcadmin/service/LivroService.java
+++ src/main/java/br/com/cdcadmin/service/LivroService.java
@@ -3,6 +3,8 @@ package br.com.cdcadmin.service;
 import java.util.List;
 import java.util.Optional;
 
+import br.com.cdcadmin.model.Autor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +21,29 @@ public class LivroService {
 		return livroRepository.save(Livro.of(livro));
 	}
 
-	public Optional<Livro> buscaPorCodigo(Long codigo) {
-		return livroRepository.findById(codigo);
+	public Optional<Livro> buscaPorId(Long id) {
+		return livroRepository.findById(id);
 	}
 	
 	public List<Livro> buscarTodos(){
 		return livroRepository.findAll();
+	}
+
+	public Livro alterarLivro (Livro livroParam, Long id){
+
+		final Optional<Livro> livro = livroRepository.findById(id);
+		BeanUtils.copyProperties(livroParam, livro);
+
+		return livroRepository.save(livroParam);
+	}
+
+	public void deletaLivro(Long id){
+		livroRepository.deleteById(id);
+	}
+
+	public Autor buscarAutorDoLivro(Long id){
+		final Optional<Livro> livro = livroRepository.findById(id);
+		return livro.get().getAutor();
 	}
 
 }
--- src/main/java/br/com/cdcadmin/web/LivroController.java
+++ src/main/java/br/com/cdcadmin/web/LivroController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import br.com.cdcadmin.model.Autor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,15 +27,36 @@ public class LivroController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(livroService.adicionaLivro(livro));
 	}
 	
-	@GetMapping("")
+	@GetMapping
 	public ResponseEntity<List<Livro>> buscarTodos() {
-		return  new ResponseEntity<List<Livro>>(livroService.buscarTodos(), HttpStatus.OK);
+		return new ResponseEntity<List<Livro>>(livroService.buscarTodos(), HttpStatus.OK);
 	}
 
-	@GetMapping("/{codigo}")
-	public ResponseEntity<Livro> buscarPeloCodigo(@PathVariable Long codigo) {
-		Optional<Livro> livro = livroService.buscaPorCodigo(codigo);
+	@GetMapping("/{id}")
+	public ResponseEntity<Livro> buscarPeloCodigo(@PathVariable Long id) {
+		Optional<Livro> livro = livroService.buscaPorId(id);
 		return livro.isPresent() ? ResponseEntity.ok(livro.get()) : ResponseEntity.notFound().build();
+	}
+
+	@GetMapping("/{id}/autor")
+	public ResponseEntity<Autor> buscarAutorDoLivro(@PathVariable Long id) {
+		return new ResponseEntity<Autor>( livroService.buscarAutorDoLivro(id), HttpStatus.OK);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Livro> alterarLivro(@RequestBody @Valid Livro livro, @PathVariable Long id) {
+		return  new ResponseEntity<Livro>(livroService.alterarLivro(livro, id), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Livro> deletaLivro(@PathVariable Long id){
+		final Optional<Livro> livro = livroService.buscaPorId(id);
+		if(livro == null){
+			return new ResponseEntity<Livro>(HttpStatus.NOT_FOUND);
+		}
+
+		livroService.deletaLivro(id);
+		return new ResponseEntity<Livro>(livro.get(), HttpStatus.NO_CONTENT);
 	}
 
 }
